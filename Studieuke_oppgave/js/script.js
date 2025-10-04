48aa4a90283f3c17797b1c137606746752634a8e
obovolden <obovolden@gmail.com>
2025-10-05 00:36:32 +0200

    Jobbet videre hjemme

--- Studieuke_oppgave/js/script.js
+++ Studieuke_oppgave/js/script.js
@@ -2,10 +2,17 @@ const burgerBtn = document.getElementById("BtnBars")
 const xBtn = document.getElementById("BtnXmark")
 const menu = document.getElementById("menu")
 
+burgerBtn.classList = false
+
 burgerBtn.addEventListener("click", () => {
     menu.classList.toggle("hidden")
+    xBtn.classList.toggle("hidden")
+    burgerBtn.classList.toggle("visible")
+
 })
 
 xBtn.addEventListener("click", () => {
     menu.classList.toggle("hidden")
+    xBtn.classList.toggle("hidden")
+    burgerBtn.classList.toggle("visible")
 })
